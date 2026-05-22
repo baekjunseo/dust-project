9f004147f8a0c63c81b16aa6c01683a03b23456d
baekjunseo <youg74@naver.com>
2026-05-22 15:16:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def load_model():
 
 @st.cache_data
 def load_data():
-     DF = pd.read_csv('final_data.csv', encoding='UTF-8-SIG')
+    DF = pd.read_csv('final_data.csv', encoding='UTF-8-SIG')
     
  
     df['datetime'] = pd.to_datetime(df['datetime'])
